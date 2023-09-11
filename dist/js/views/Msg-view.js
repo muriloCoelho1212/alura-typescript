d0d9df49b42e5b87eabc47eeac9ea7ade74df5a8
Murilo <m.coelhos1212@gmail.com>
2023-09-11 15:00:46 -0300

    deletando pasta dist fora da pasta app

--- dist/js/views/Msg-view.js
+++
@@ -1,8 +0,0 @@
-import { View } from "./View.js";
-export class MsgView extends View {
-    template(model) {
-        return `
-            <p class="alert alert-info">${model}</p>
-        `;
-    }
-}
