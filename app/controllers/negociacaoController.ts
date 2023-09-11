963db015284a6e20bea4b5e55447873df8a98651
Murilo <m.coelhos1212@gmail.com>
2023-09-11 12:44:52 -0300

    commit final aula 4
    Nesta aula, aprendemos:
    
        Revisão da lógica de conversão negociações
        Método estáticos
        Parâmetros opcionais

--- app/controllers/negociacaoController.ts
+++ app/controllers/negociacaoController.ts
@@ -20,7 +20,7 @@ export class NegociacaoController {
     }
 
     public add(): void{
-        const negociacao = this.criaNegociacao()
+        const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value)
         if(!this.diaUtil(negociacao.data)){
             this.msgView.update("Apenas negociações em dias úteis são aceitas.")
             return
@@ -32,14 +32,6 @@ export class NegociacaoController {
 
     private diaUtil(data: Date){
         return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO
-    }
-
-    private criaNegociacao(): Negociacao{
-        const exp = /-/g
-        const date = new Date(this.inputData.value.replace(exp, "/"))
-        const quantidade = parseInt(this.inputQuantidade.value)
-        const valor = parseFloat(this.inputValor.value)
-        return new Negociacao(date, quantidade, valor)
     }
 
     private limparForm(): void {
